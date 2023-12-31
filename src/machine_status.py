d6c55547d88fc28ed12818e8bb88928ce32ed050
Carlos Tovar <carlos.mercury92@gmail.com>
2023-12-31 14:58:19 -0600

    Machine status added publishing and subscribing

--- src/machine_status.py
+++ src/machine_status.py
@@ -1,4 +1,5 @@
 import threading
+import json
 from mqtt_client import MqttClient
 
 laser_doors = False
@@ -26,38 +27,39 @@ class MachineStatus:
         self.mqtt_client.connect()
         self.mqtt_client.subscribe(TOPICS)
 
-    def on_message_callback(self, client, userdata, msg):
+    @staticmethod
+    def on_message_callback(client, userdata, msg):
 
         global laser_doors, smoke_extractor, material_workspace, available_space, leds_strip, laser_camera
 
-        client_id = self.mqtt_client.get_client_id()
+        # client_id = self.mqtt_client.get_client_id()
         # Ignore messages sent by this client
-        if msg.topic == f"your/topic/{client_id}":
-            print("Ignoring message sent by this client.")
-            return
+        # if msg.topic == f"your/topic/{client_id}":
+            # print("Ignoring message sent by this client.")
+            # return
 
         if msg.topic == 'machine_status/laser_doors':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             laser_doors = payload_message['laser_doors']['status']
 
         if msg.topic == 'machine_status/smoke_extractor':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             smoke_extractor = payload_message['smoke_extractor']['status']
 
         if msg.topic == 'machine_status/material_workspace':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             material_workspace = payload_message['material_workspace']['status']
 
         if msg.topic == 'machine_status/available_space':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             available_space = payload_message['available_space']['status']
 
         if msg.topic == 'machine_status/leds_strip':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             leds_strip = payload_message['leds_strip']['status']
 
         if msg.topic == 'machine_status/laser_camera':
-            payload_message = msg.payload.decode
+            payload_message = json.loads(msg.payload.decode())
             laser_camera = payload_message['laser_camera']['status']
 
     def laser_doors_status(self):
@@ -88,17 +90,41 @@ class MachineStatus:
 def run():
     global laser_doors, smoke_extractor, material_workspace, available_space, leds_strip, laser_camera
 
-    while not laser_doors and \
-            not smoke_extractor and \
-            not material_workspace and \
-            not available_space and not leds_strip and \
-            not laser_camera:
-        return True
-
-
-if __name__ == '__main__':
     machine_status = MachineStatus("192.168.0.23", broker_port=1883)
-    # Create a daemon thread
-    daemon_thread = threading.Thread(target=run)
-    daemon_thread.daemon = True
-    daemon_thread.start()
+
+    while True:
+
+        if not laser_doors:
+            machine_status.laser_doors_status()
+
+        if laser_doors and not smoke_extractor:
+            machine_status.smoke_extractor_status()
+
+        if laser_doors and smoke_extractor and not material_workspace:
+            machine_status.material_workspace_status()
+
+        if laser_doors and smoke_extractor and material_workspace and not available_space:
+            machine_status.available_space_status()
+
+        if laser_doors and smoke_extractor and material_workspace and available_space and not leds_strip:
+            machine_status.leds_strip_status()
+
+        if laser_doors and smoke_extractor and material_workspace and \
+                available_space and leds_strip and not laser_camera:
+            machine_status.laser_camera_status()
+
+        if laser_doors and smoke_extractor and material_workspace and available_space and \
+                leds_strip and laser_camera:
+            machine_status_message_ = 'machine is ready to run'
+            break
+    return machine_status_message_
+
+
+"""
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/laser_doors" -m '{"laser_doors": {"status": true}}'
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/smoke_extractor" -m '{"smoke_extractor": {"status": true}}'
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/material_workspace" -m '{"material_workspace": {"status": true}}'
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/available_space" -m '{"available_space": {"status": true}}'
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/leds_strip" -m '{"leds_strip": {"status": true}}'
+mosquitto_pub -h 192.168.0.23 -p 1883 -t "machine_status/laser_camera" -m '{"laser_camera": {"status": true}}'
+"""
