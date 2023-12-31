768208d0d290d4e31893232cebb02ed0cb732d60
Carlos Tovar <carlos.mercury92@gmail.com>
2023-12-31 12:02:11 -0600

    Adding mqtt client machine status

---
+++ src/machine_status.py
@@ -0,0 +1,103 @@
+import threading
+from mqtt_client import MqttClient
+
+laser_doors = False
+smoke_extractor = False
+material_workspace = False
+available_space = False
+leds_strip = False
+laser_camera = False
+TOPICS = [
+    'machine_status/laser_doors',
+    'machine_status/smoke_extractor',
+    'machine_status/material_workspace',
+    'machine_status/available_space',
+    'machine_status/leds_strip',
+    'machine_status/laser_camera'
+]
+
+
+class MachineStatus:
+
+    def __init__(self, mqtt_server, broker_port):
+        self.mqtt_client = MqttClient(mqtt_server,
+                                      broker_port=broker_port,
+                                      on_message_callback=self.on_message_callback)
+        self.mqtt_client.connect()
+        self.mqtt_client.subscribe(TOPICS)
+
+    def on_message_callback(self, client, userdata, msg):
+
+        global laser_doors, smoke_extractor, material_workspace, available_space, leds_strip, laser_camera
+
+        client_id = self.mqtt_client.get_client_id()
+        # Ignore messages sent by this client
+        if msg.topic == f"your/topic/{client_id}":
+            print("Ignoring message sent by this client.")
+            return
+
+        if msg.topic == 'machine_status/laser_doors':
+            payload_message = msg.payload.decode
+            laser_doors = payload_message['laser_doors']['status']
+
+        if msg.topic == 'machine_status/smoke_extractor':
+            payload_message = msg.payload.decode
+            smoke_extractor = payload_message['smoke_extractor']['status']
+
+        if msg.topic == 'machine_status/material_workspace':
+            payload_message = msg.payload.decode
+            material_workspace = payload_message['material_workspace']['status']
+
+        if msg.topic == 'machine_status/available_space':
+            payload_message = msg.payload.decode
+            available_space = payload_message['available_space']['status']
+
+        if msg.topic == 'machine_status/leds_strip':
+            payload_message = msg.payload.decode
+            leds_strip = payload_message['leds_strip']['status']
+
+        if msg.topic == 'machine_status/laser_camera':
+            payload_message = msg.payload.decode
+            laser_camera = payload_message['laser_camera']['status']
+
+    def laser_doors_status(self):
+        status = {'laser_doors': {'status': False}}
+        self.mqtt_client.publish("machine_status/laser_doors", status)
+
+    def smoke_extractor_status(self):
+        status = {'smoke_extractor': {'status': False}}
+        self.mqtt_client.publish("machine_status/smoke_extractor", status)
+
+    def material_workspace_status(self):
+        status = {'material_workspace': {'status': False}}
+        self.mqtt_client.publish("machine_status/material_workspace", status)
+
+    def available_space_status(self):
+        status = {'available_space': {'status': False}}
+        self.mqtt_client.publish("machine_status/available_space", status)
+
+    def leds_strip_status(self):
+        status = {'leds_strip': {'status': False}}
+        self.mqtt_client.publish("machine_status/leds_strip", status)
+
+    def laser_camera_status(self):
+        status = {'laser_camera': {'status': False}}
+        self.mqtt_client.publish("machine_status/laser_camera", status)
+
+
+def run():
+    global laser_doors, smoke_extractor, material_workspace, available_space, leds_strip, laser_camera
+
+    while not laser_doors and \
+            not smoke_extractor and \
+            not material_workspace and \
+            not available_space and not leds_strip and \
+            not laser_camera:
+        return True
+
+
+if __name__ == '__main__':
+    # Create a daemon thread
+    daemon_thread = threading.Thread(target=run)
+    daemon_thread.daemon = True
+    daemon_thread.start()
--- src/mqtt_client.py
+++ src/mqtt_client.py
@@ -27,13 +27,12 @@ Mqtt client subscriber
 
 """
 import time
-
 import paho.mqtt.client as mqtt
 
 
 class MqttClient:
 
-    def __init__(self, broker_address, broker_port=1883, username=None, password=None):
+    def __init__(self, broker_address, broker_port: int = 1883, username=None, password=None, on_message_callback=None):
         self.client = mqtt.Client()
         self.broker_address = broker_address
         self.broker_port = broker_port
@@ -43,17 +42,28 @@ class MqttClient:
         # Set callbacks
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
-        self.client.on_message = self.on_message
+
+        # Set on_message callback dynamically
+        if on_message_callback:
+            self.client.on_message = on_message_callback
+        else:
+            self.client.on_message = self.default_on_message
+
+        self.client_id = None
 
     def on_connect(self, client, userdata, flags, rc):
         print(self.validate_connection)
         print(f"Connected with result code {rc}")
+        self.client_id = client._client_id.decode()
 
     def on_disconnect(self, client, userdata, rc):
         print(self.validate_connection)
         print(f"Disconnected with result code {rc}")
 
-    def on_message(self, client, userdata, msg):
+    def default_on_message(self, client, userdata, msg):
+        if msg.topic == f"your/topic/{self.client_id}":
+            print("Ignoring message sent by this client.")
+            return
         print(self.validate_connection)
         print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
 
@@ -69,17 +79,26 @@ class MqttClient:
     def publish(self, topic, message):
         self.client.publish(topic, message)
 
-    def subscribe(self, topic):
-        self.client.subscribe(topic)
+    def subscribe(self, topics):
+        for topic in topics:
+            self.client.subscribe(topic)
 
     def validate_connection(self):
         return self.client.is_connected()
 
+    def get_client_id(self):
+        return self._client_id
+
 
 # Example usage:
-mqtt_client = MqttClient("192.168.0.23", broker_port=1883)  # , username="Routiva_Laser", password="your_password")
-mqtt_client.connect()
-mqtt_client.subscribe("test/topic")
-mqtt_client.publish("test/topic", "Hello I am Charly Mercury from laser")
-time.sleep(1)
-mqtt_client.disconnect()
+# Define a custom on_message callback
+# def custom_on_message(client, userdata, msg):
+#     print(f"Custom on_message: Received message on topic {msg.topic}: {msg.payload.decode()}")
+# Example usage:
+# mqtt_client = MqttClient("192.168.0.23", broker_port=1883,
+# username="Routiva_Laser", password="your_password", on_message_callback=custom_on_message)
+# mqtt_client.connect()
+# mqtt_client.subscribe("test/topic")
+# mqtt_client.publish("test/topic", "Hello I am Charly Mercury from laser")
+# time.sleep(1)
+# mqtt_client.disconnect()
