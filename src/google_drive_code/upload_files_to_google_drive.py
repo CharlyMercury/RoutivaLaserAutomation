fec5ea601f10d7ab58d7c3f74a07d64cbcfadb7a
Carlos Tovar <carlos.mercury92@gmail.com>
2024-03-27 23:22:58 -0600

    Rasbperry Configuration - usb devices - usb parameters - Gdrive

--- src/google_drive_code/upload_files_to_google_drive.py
+++ src/google_drive_code/upload_files_to_google_drive.py
@@ -8,7 +8,7 @@ import os
 # Below code does the authentication
 # part of the code
 gauth = GoogleAuth()
-gauth.LoadCredentialsFile("./src/google_drive_code/my_creds.json")
+gauth.LoadCredentialsFile("./my_creds.json")
 
 if gauth.credentials is None:
     # Authenticate if they're not there
@@ -19,7 +19,7 @@ if gauth.credentials is None:
     gauth.LocalWebserverAuth()
 elif gauth.access_token_expired:
     # Delete my_creds.json file before to make the Refresh
-    os.remove("./src/google_drive_code/my_creds.json")
+    os.remove("./my_creds.json")
     # Refresh them if expired
     gauth.Refresh()
     gauth.Authorize()
@@ -27,7 +27,7 @@ else:
     # Initialize the saved creds
     gauth.Authorize()
 # Save the current credentials to a file
-gauth.SaveCredentialsFile("./src/google_drive_code/my_creds.json")
+gauth.SaveCredentialsFile("./my_creds.json")
 
 # Creates local webserver and auto
 # handles authentication.
@@ -36,23 +36,26 @@ drive = GoogleDrive(gauth)
 # replace the value of this variable
 # with the absolute path of the directory
 # upload_file_list = [r'./src/google_drive_code/proposed_architecture.png', r'./src/google_drive_code/proposed_architecture_2.png']
-upload_file_list = [r'./src/google_drive_code/proposed_architecture_2.png']
+upload_file_list = [r'Corte.gcode']
 folder_id = '1Clv8oI2A3zdSZeqg5oFlXGLsxFN6GhKL'
 
 # iterating thought all the files/folder
 # of the desired directory
 for x in upload_file_list:
-    # file_ = drive.CreateFile({'parents': [{'id': folder_id}]})
-    # file_.SetContentFile(x)
-    # file_.Upload()
+    file_ = drive.CreateFile({'parents': [{'id': folder_id}]})
+    file_.SetContentFile(x)
+    file_.Upload()
 
-    # Due to a known bug in pydrive if we
-    # don't empty the variable used to
-    # upload the files to Google Drive the
-    # file stays open in memory and causes a
-    # memory leak, therefore preventing its
-    # deletion
+    """
+    Due to a known bug in pydrive if we
+    don't empty the variable used to
+    upload the files to Google Drive the
+    file stays open in memory and causes a
+    memory leak, therefore preventing its
+    deletion        
+    """
 
+    """
     file_list = drive.ListFile(
         {'q': "'{}' in parents and trashed=false".format(folder_id)}).GetList()
 
@@ -63,3 +66,5 @@ for x in upload_file_list:
         print('Downloading {} file from GDrive ({}/{})'.format(file_3['title'], i, len(file_list)))
         print(file_3['title'])
         file_3.GetContentFile(file_3['title'])
+    """
+
--- src/mqtt_server_client.py
+++ src/mqtt_server_client.py
@@ -85,43 +85,10 @@ class MqttServerBrokerClient:
 
             if validation_status and validation_error == "No errors":
                 self.mqtt_client.publish(publishing_topics["confirmation_trigger_cutting"], "Initializing Cutting Process")
+                self.mqtt_client.disconnect()
             if not validation_status:
                 self.mqtt_client.publish(publishing_topics["confirmation_trigger_cutting"], validation_error)
 
         if msg.topic == 'routiva_mqtt_broker/confirmation_status_machine':
 
             print('')
-
-
-"""message = {
-    "machine_name": "sculpfun_s30_90_90", 
-    "mdf_type": "natural_mdf",
-    "file_name": "pedido_31_ago.gcode",
-    "folder_id": "1Clv8oI2A3zdSZeqg5oFlXGLsxFN6GhKL",
-    "credentials": 
-        {"access_token": "ya29.a0Ad52N39FPIly7SfyGzckS25yYgm6_ZaDE2IBmhj_Pbksu0TmJOxXVeWZPOq8VDsxq4c_VdbFc8rYJXFaONrtzAWl3P3zWerTgQVejq-pWhij6nctpOftDwU_pEDLtc3tPdQCPo_cpdNrgpVVH-skugsDtnxTLnc7WV0vaCgYKAfESARASFQHGX2Mi42C2Cgpdt3lJ92ROtYr_5A0171",
-        "client_id": "965793021308-sim1mdfeke46riovdc6kg4pl5mtmntt3.apps.googleusercontent.com",
-        "client_secret": "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",
-        "refresh_token": "1//0f9aBDF0SNUqoCgYIARAAGA8SNwF-L9IrK7oHGXS5sCwwsblyhFdc8O2KEwC5NJ2zzT2_7Wv8hsVr4R1D_OZNBxIvw4ros4g7Loo",
-        "token_expiry": "2024-03-22T16:58:19Z",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "user_agent": None,
-        "revoke_uri": "https://oauth2.googleapis.com/revoke",
-        "id_token": None,
-        "id_token_jwt": None,
-        "token_response": {
-            "access_token": "ya29.a0Ad52N39FPIly7SfyGzckS25yYgm6_ZaDE2IBmhj_Pbksu0TmJOxXVeWZPOq8VDsxq4c_VdbFc8rYJXFaONrtzAWl3P3zWerTgQVejq-pWhij6nctpOftDwU_pEDLtc3tPdQCPo_cpdNrgpVVH-skugsDtnxTLnc7WV0vaCgYKAfESARASFQHGX2Mi42C2Cgpdt3lJ92ROtYr_5A0171",
-            "expires_in": 3599,
-            "refresh_token": "1//0f9aBDF0SNUqoCgYIARAAGA8SNwF-L9IrK7oHGXS5sCwwsblyhFdc8O2KEwC5NJ2zzT2_7Wv8hsVr4R1D_OZNBxIvw4ros4g7Loo",
-            "scope": "https://www.googleapis.com/auth/drive",
-            "token_type": "Bearer"
-        },
-        "scopes": [
-            "https://www.googleapis.com/auth/drive"
-        ],
-        "token_info_uri": "https://oauth2.googleapis.com/tokeninfo",
-        "invalid": False,
-        "_class": "OAuth2Credentials",
-        "_module": "oauth2client.client"
-        }
-}"""
