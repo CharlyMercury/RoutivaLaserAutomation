dfa34535a532ec1fc8dd97fc92618328ff17be36
CharlyMercury <carlos.mercury92@gmail.com>
2024-04-06 20:56:50 +0100

    adding starth

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ~/RoutivaLaserAutomation/venv/bin/activate
+python ~/RoutivaLaserAutomation/main.py
+
+#!/bin/bash
+cd RoutivaLaserAutomation
+source venv/bin/activate
+python3 main.py
